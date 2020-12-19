ed79beaf0ef00245716052e1896f59476b346cd8
logan preston <personal@loganpreston.com>
2020-12-18 19:10:29 -0600

    add EJS template and practice using templating

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-const express=require("express");
-const bodyParser=require("body-parser");
-const https=require("https");
+const express = require("express");
+const bodyParser = require("body-parser");
+//const ejs = require("ejs");
+const https = require("https");
 
-const app=express();
+const app = express();
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
-app.get("/",function(req,res){
-    res.send("Hello");
+daysOfWeek=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+app.get("/", function (req, res) {
+  var today = new Date();
+  var day = daysOfWeek[today.getDay()];
+  /*
+  if (today.getDay() === 6 || today.getDay() === 0) {
+    day = "Weekend";
+    //res.send("Yay it's the weekend!");
+  } else {
+    day = "Weekday";
+    //res.write("<p>Nay it's the work week!</p>");
+    //res.write("<h1>another line</h1>")
+    //res.sendFile(__dirname+"/index.html");
+  }
+  */
+  res.render("list", { kindOfDay: day });
 });
 
-app.listen(3000,function(){
-    console.log("Server started on port 3000");
-})
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
