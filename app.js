5898ab1d637c82e15025dd13f7b2af8ee4941b39
logan preston <personal@loganpreston.com>
2020-12-19 22:20:09 -0600

    add about page, use ejs for header and footer

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 let items = ["Buy Food","Cook Food","Eat Food"];
+let workItems = [];
+
 
 app.get("/", function (req, res) {
   let today = new Date();
@@ -19,14 +21,29 @@ app.get("/", function (req, res) {
   };
   let day = today.toLocaleDateString("en-US", options); 
   
-  res.render("list", { kindOfDay: day, newListItems: items });
+  res.render("list", { listTitle: day, newListItems: items });
   
 });
 
+app.get("/work",function(req,res){
+    res.render("list",{listTitle:"Work List",newListItems:workItems})
+});
+
+app.get("/about",function(req,res){
+    res.render("about");
+})
+
 app.post("/", function (req, res) {
-  let item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+    let item = req.body.newItem;
+    
+    if(req.body.list==="Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else{
+        items.push(item);
+        res.redirect("/");
+    }
 });
 
 app.listen(3000, function () {
