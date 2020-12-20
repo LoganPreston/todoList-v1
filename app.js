df1bed936684b54d4cc7e4ed81c5c06966aa1609
logan preston <personal@loganpreston.com>
2020-12-19 22:30:28 -0600

    add date.js and call date using require

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-//const ejs = require("ejs");
+const date = require(__dirname + "/date.js");
 const https = require("https");
 
 const app = express();
@@ -8,42 +8,33 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-let items = ["Buy Food","Cook Food","Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
 let workItems = [];
 
-
 app.get("/", function (req, res) {
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-  let day = today.toLocaleDateString("en-US", options); 
-  
+  let day = date();
+
   res.render("list", { listTitle: day, newListItems: items });
-  
 });
 
-app.get("/work",function(req,res){
-    res.render("list",{listTitle:"Work List",newListItems:workItems})
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
-app.get("/about",function(req,res){
-    res.render("about");
-})
+app.get("/about", function (req, res) {
+  res.render("about");
+});
 
 app.post("/", function (req, res) {
-    let item = req.body.newItem;
-    
-    if(req.body.list==="Work"){
-        workItems.push(item);
-        res.redirect("/work");
-    }
-    else{
-        items.push(item);
-        res.redirect("/");
-    }
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 });
 
 app.listen(3000, function () {
